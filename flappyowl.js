4d8a6a81d09aab493e118e64e6d726cb12764523
HouseMushrooms <djholliday789@gmail.com>
2024-03-18 22:30:42 -0400

    drew bird onto board + fixed trashcan images

--- flappyowl.js
+++ flappyowl.js
@@ -4,10 +4,49 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
+//bird
+let birdWidth = 56; //width/height ration = 280/310 = 28/31
+let birdHeight = 62;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+let birdImg;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
+//pipes
+let pipeArray = [];
+let pipeWidth = 66; //width/height ratio = 110/920 = 11/92
+let pipeHeight = 552;
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); //used to draw on the board
 
+    //draw flappy owl
+   // context.fillStyle = "blue";
+    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    //load images
+    birdImg = new Image();
+    birdImg.src = "./flappyowl.png";
+    birdImg.onload = function(){
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    requestAnimationFrame(update);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    //bird
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 }
