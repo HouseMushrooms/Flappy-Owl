0db3ac0e0d30218660130688b758700d311b32b9
HouseMushrooms <djholliday789@gmail.com>
2024-03-20 21:21:38 -0400

    Physics and Gameplay (mostly done)

--- flappyowl.js
+++ flappyowl.js
@@ -22,6 +22,19 @@ let bird = {
 let pipeArray = [];
 let pipeWidth = 66; //width/height ratio = 110/920 = 11/92
 let pipeHeight = 552;
+let pipeX = boardWidth; 
+let pipeY = 0;
+
+let topTrashcanImg;
+let bottomTrashcanImg;
+
+//physics
+let velocityX = -2; //pipes moving left speed
+let velocityY = 0; //bird jump speed
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -40,13 +53,109 @@ window.onload = function(){
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
+    topTrashcanImg = new Image();
+    topTrashcanImg.src = "./toptrashcan.png";
+
+    bottomTrashcanImg = new Image();
+    bottomTrashcanImg.src = "./bottomtrashcan.png";
+
     requestAnimationFrame(update);
+    setInterval(placeTrashcans, 1500); //every 1.5 seconds
+    document.addEventListener("keydown", moveBird);
+    document.addEventListener("click", moveBird);
 }
 
 function update() {
     requestAnimationFrame(update);
+    if (gameOver){
+        return;
+    }
     context.clearRect(0, 0, board.width, board.height);
 
     //bird
+    velocityY += gravity;
+   //bird.y += velocityY;
+    bird.y = Math.max(bird.y + velocityY, 0); //apply gravity to current bird.y, limit the bird.y to top of the canvas
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    if(bird.y > board.height-60){
+        gameOver = true;
+    }
+
+    //pipes
+    for (let i = 0; i < pipeArray.length; i++){
+        let trashcan = pipeArray[i];
+        trashcan.x += velocityX;
+        context.drawImage(trashcan.img, trashcan.x, trashcan.y, trashcan.width, trashcan.height);
+
+        if(!trashcan.passed && bird.x > trashcan.x + trashcan.width){
+            score += 0.5; //2 (trahscans)*0.5 = 1 score per set of pipes
+            trashcan.passed = true;
+        }
+
+        if (detectCollision(bird, trashcan)){
+            gameOver = true;
+        }
+    }
+   //clear
+   while(pipeArray.length > 0 && pipeArray[0].x < -pipeWidth-10){
+        pipeArray.shift(); //remove first element from the array
+   }
+
+    //score
+    context.fillStyle = "white";
+    context.font = "45px Helvetica";
+    context.fillText(score, 5, 45);
+
+    if(gameOver){
+        context.fillText("GAME OVER", 45, boardHeight/2);
+    }
+}
+
+function placeTrashcans(){
+    if (gameOver){
+        return;
+    }
+    //(0-1) * pipeHeight/2
+    //0 -> -138 (pipeHeight/4)
+    //1 -> - 138 - 276 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = board.height/4;
+
+    let topTrashcan = {
+        img : topTrashcanImg,
+        x : pipeX,
+        y : randomPipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    pipeArray.push(topTrashcan);
+
+    let bottomTrashcan = {
+        img : bottomTrashcanImg,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + openingSpace,
+        width :pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    pipeArray.push(bottomTrashcan);
+}
+
+function moveBird(e){
+        velocityY = -6;
+
+    //reset game
+    if(gameOver){
+        
+    }
+}
+
+function detectCollision(a,b){
+    return  a.x < b.x + b.width-10 &&
+            a.x + a.width-15 > b.x &&
+            a.y < b.y + b.height-10 &&
+            a.y + a.height-25 > b.y;
+
 }
